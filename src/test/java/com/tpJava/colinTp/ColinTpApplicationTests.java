23c755baf6e13d85c49b96b837bae7bae89f4694
casoarctik <colin.beaumont@outlook.fr>
2020-12-11 00:07:22 +0100

    init repository

---
+++ src/test/java/com/tpJava/colinTp/ColinTpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tpJava.colinTp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColinTpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

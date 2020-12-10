826c0d9743bdf29e54f59f0c2f8247c8e08c2cf8
casoarctik <colin.beaumont@outlook.fr>
2020-12-11 00:15:30 +0100

    add model entity (test git)

---
+++ src/main/java/com/tpJava/colinTp/model/Album.java
@@ -0,0 +1,9 @@
+package com.tpJava.colinTp.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "album")
+public class Album {
+}
---
+++ src/main/java/com/tpJava/colinTp/model/Artist.java
@@ -0,0 +1,9 @@
+package com.tpJava.colinTp.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "artist")
+public class Artist {
+}
